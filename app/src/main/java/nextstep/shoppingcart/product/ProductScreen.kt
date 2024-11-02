79352f2e0bd12f2013b509fe2f393d32b4c4406a
murjune <sis9221302@gmail.com>
2024-11-03 05:30:32 +0900

    refactor: ProductItem 컴포넌트 분리

--- app/src/main/java/nextstep/shoppingcart/product/ProductScreen.kt
+++ app/src/main/java/nextstep/shoppingcart/product/ProductScreen.kt
@@ -1,21 +1,12 @@
 package nextstep.shoppingcart.product
 
-import androidx.compose.foundation.border
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ShoppingCart
 import androidx.compose.material3.CenterAlignedTopAppBar
@@ -28,17 +19,13 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.tooling.preview.PreviewParameter
 import androidx.compose.ui.unit.dp
-import coil.compose.AsyncImage
 import nextstep.shoppingcart.R
 import nextstep.shoppingcart.domain.model.Product
+import nextstep.shoppingcart.product.component.ProductItem
 import nextstep.shoppingcart.product.preview.ProductPreviewParameterProvider
 import nextstep.shoppingcart.ui.theme.ShoppingCartTheme
 
@@ -74,7 +61,7 @@ fun ProductScreen(
             )
         }
         ProductContent(
-            producs = products,
+            products = products,
             modifier = Modifier
                 .padding(innerPadding)
                 .padding(horizontal = 18.dp),
@@ -86,7 +73,7 @@ fun ProductScreen(
 
 @Composable
 private fun ProductContent(
-    producs: List<Product>,
+    products: List<Product>,
     modifier: Modifier = Modifier,
     onItemClick: (id: Long) -> Unit,
 ) {
@@ -96,7 +83,7 @@ private fun ProductContent(
         verticalArrangement = Arrangement.spacedBy(20.dp),
         horizontalArrangement = Arrangement.spacedBy(12.dp)
     ) {
-        items(producs, key = { it.id }) { product ->
+        items(products, key = { it.id }) { product ->
             ProductItem(
                 product,
                 onClick = onItemClick
@@ -119,65 +106,6 @@ private fun ProductEmptyContent(
         )
     }
 
-}
-
-@Composable
-private fun ProductItem(
-    product: Product,
-    onClick: (id: Long) -> Unit,
-    modifier: Modifier = Modifier,
-) {
-    Column(
-        modifier = modifier.clickable {
-            onClick(product.id)
-        }
-    ) {
-        AsyncImage(
-            modifier = Modifier
-                .fillMaxWidth()
-                .aspectRatio(1f)
-                .clip(RoundedCornerShape(16.dp))
-                .border(
-                    width = 1.dp,
-                    color = Color.Black,
-                    shape = RoundedCornerShape(16.dp)
-                ),
-            model = product.imageUrl,
-            contentDescription = "상품 이미지",
-        )
-        Spacer(modifier = Modifier.height(8.dp))
-        Column(Modifier.padding(horizontal = 4.dp)) {
-            Text(
-                text = product.name,
-                maxLines = 1,
-                overflow = TextOverflow.Ellipsis,
-                style = MaterialTheme.typography.labelLarge.copy(fontWeight = FontWeight.Bold),
-            )
-            Spacer(modifier = Modifier.height(2.dp))
-            Text(
-                text = stringResource(id = R.string.price_format, product.price),
-                style = MaterialTheme.typography.bodySmall
-            )
-        }
-    }
-}
-
-@Preview(showBackground = true)
-@Composable
-private fun PreviewProductItem() {
-    ShoppingCartTheme {
-        ProductItem(
-            product = Product(
-                id = 1,
-                name = "상품 이름",
-                price = 1000,
-                imageUrl = "https://www.example.com/image.jpg"
-
-            ),
-            onClick = {},
-            modifier = Modifier.size(100.dp, 150.dp)
-        )
-    }
 }
 
 @Preview(showBackground = true)
